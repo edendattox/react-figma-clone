5536fec39516f46f7e83db0ad62d50eddce4a855
rahul kathait <edendattox@gmail.com>
2022-01-15 19:09:53 +0530

    Added template svg

---
+++ src/icons/Template.js
@@ -0,0 +1,89 @@
+import * as React from "react";
+
+export const SvgTemplate = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    className="template_svg__figma"
+    viewBox="0 0 248 160"
+    width="248"
+    {...props}
+  >
+    <g clip-path="url(#clip0)">
+      <path fill="#5551FF" d="M0 0h248v160H0z"></path>
+      <rect
+        width="109"
+        height="123"
+        x="98"
+        y="125"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+        rx="5"
+      ></rect>
+      <path fill="#0FA958" d="M110 140h85v57h-85z"></path>
+      <rect
+        width="109"
+        height="123"
+        x="54"
+        y="61"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+        rx="5"
+      ></rect>
+      <path fill="#0FA958" d="M66 76h85v57H66z"></path>
+      <path fill="#0FA958" d="M66 144h85v19H66z"></path>
+      <path
+        fill="#FFC700"
+        stroke="#000"
+        stroke-width="2"
+        d="M102 24h68v71h-68z"
+      ></path>
+      <circle
+        cx="103"
+        cy="24"
+        r="5"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+      ></circle>
+      <circle
+        cx="103"
+        cy="95"
+        r="5"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+      ></circle>
+      <circle
+        cx="170"
+        cy="24"
+        r="5"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+      ></circle>
+      <circle
+        cx="170"
+        cy="95"
+        r="5"
+        fill="#fff"
+        stroke="#000"
+        stroke-width="2"
+      ></circle>
+      <path
+        fill="#5551FF"
+        stroke="#000"
+        stroke-linecap="square"
+        stroke-width="2"
+        d="M128.2 102.2l.5 2.6 1.4-2.3 6-10.4 11.2-3.1 2.2-.7-2-1.2-23-14-1.9-1.1.4 2.2 5.2 28z"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <path fill="#fff" d="M0 0h248v160H0z"></path>
+      </clipPath>
+    </defs>
+  </svg>
+);
