6de58eee237c16ef86111b78d7ff0fd40e4a2ed5
rahul kathait <edendattox@gmail.com>
2022-01-18 13:59:39 +0530

    Added Youtube icon

---
+++ src/icons/YouTube.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const SvgYouTube = (props) => (
+  <svg width="16" height="13" className="figma-holhpz" {...props}>
+    <path d="M15.6 2c.4 1.4.4 4.4.4 4.4s0 3-.4 4.4c-.2.8-.8 1.4-1.5 1.6-1.4.4-6.1.4-6.1.4s-4.7 0-6-.4c-.8-.2-1.4-.8-1.6-1.6C0 9.4 0 6.4 0 6.4S0 3.4.4 2C.6 1.2 1.2.6 1.9.4 3.3 0 8 0 8 0s4.7 0 6 .4c.8.2 1.4.8 1.6 1.6zM6.4 9.2l4.8-2.8-4.8-2.8v5.6z"></path>
+  </svg>
+);
