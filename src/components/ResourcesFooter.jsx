ce9631757fc44c08fa745909be30b74bd4d2fdc8
rahul kathait <edendattox@gmail.com>
2022-01-18 13:56:10 +0530

    Added Resources Footer

---
+++ src/components/ResourcesFooter.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const ResourcesFooter = () => {
+  return (
+    <div>
+      <h6 className="text-[14px] text-black font-bold my-[16px] text-left">
+        Resources
+      </h6>
+      <div className="flex flex-col items-start">
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Blog
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Best practices
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Support
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Developers
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Learn design
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Downloads
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          What's new
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Releases
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Careers
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          About us
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Agency partners
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Privacy
+        </Link>
+        <Link
+          to="/"
+          className="text-[14px] leading-[1.5] tracking-wider whitespace-nowrap mb-[16px] text-[#282828]
+        hover:underline underline-offset-1
+        "
+        >
+          Status
+        </Link>
+      </div>
+    </div>
+  );
+};
